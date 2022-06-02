2a4029ecea4be9ed678c459e342e389060f7339c
Will Robson Monteiro Rocha <willrocha@MacBook-Air.local>
2022-06-02 21:54:56 +0200

    commit/binder

--- Pyevolve/build_dist.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-python setup.py build
-
-python setup.py bdist_wininst --target-version="2.5"
-python setup.py bdist_wininst --target-version="2.6"
-python setup.py sdist --formats=gztar,zip
-python setup.py bdist --formats=egg
-
-
